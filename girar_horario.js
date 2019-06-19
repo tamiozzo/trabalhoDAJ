44ded4ade00b20cdc575b81ad1cb03e0bd8acac7
tamiozzo <gabriel.tamiozzo@gmail.com>
2019-06-18 22:40:11 -0300

    Update girar_horario.js
    correção da girada horario e anti-horario
    
    tem que atualizar tudo.

--- girar_horario.js
+++ girar_horario.js
@@ -1,17 +1,17 @@
 window.onload = function(){
-	cores = ['#999', '#03f', '#ff6', '#ff0', '#60c', '#F00'];
+	cores = ['#999', '#03f', '#ff6', '#F00', '#60c', '#ff0'];
 
 	elementos = [document.getElementsByClassName("es")[0], 
 	document.getElementsByClassName("pr")[0], 
-	document.getElementsByClassName("cl")[0], 
-	document.getElementsByClassName("bo")[0], 
-	document.getElementsByClassName("ca")[0], 
-	document.getElementsByClassName("pro")[0]];
-	trocaCoresHorario(1);
+	document.getElementsByClassName("cl")[0],
+	document.getElementsByClassName("pro")[0], 
+	document.getElementsByClassName("ca")[0],
+	document.getElementsByClassName("bo")[0] 
+	];
+	trocaCoresAntiHorario(1);
+	//trocaCoresHorario(1);
 
-
-
-	function trocaCoresHorario(vez){
+	function trocaCoresAntiHorario(vez){
 		i = 0;
 		for(elemento of elementos){
 			elemento.style.backgroundColor = cores[vez+i];
@@ -31,6 +31,28 @@ window.onload = function(){
 			vez = 0;
 		}
 		setTimeout(() => {trocaCoresHorario(vez)}, 1000);
+	}
+
+	function trocaCoresHorario(vez){
+		i = 0;
+		for(elemento of elementos){
+			elemento.style.backgroundColor = cores[5- (vez+i)];
+			console.log("valor de vez = "+ vez);
+			console.log("valor de i = " + i);
+			console.log("valor de 5-(vez+i) =" + (5- (vez+i)));
+			if(vez+i > 0){
+				i--;
+			}else{
+				console.log("chegou no 0");
+				i = 5 - vez;
+			}
+		}
+		if (vez < 5) {
+			vez++;
+		}else{
+			vez = 0;
+		}
+		setTimeout(() => {trocaCoresAntiHorario(vez)}, 1000);
 	}
 
 };
